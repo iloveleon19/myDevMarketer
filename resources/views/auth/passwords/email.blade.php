8707ed21b48bd1e535306d6b067951a31ec2aa43
iloveleon19 <iloveleon19@gmail.com>
2018-06-24 14:31:51 +0800

    EP-9

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,12 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+  @if (session('status'))
+    <div class="notification is-success">
+        {{ session('status') }}
+    </div>
+  @endif
+
+<div class="columns">
+  <div class="column is-one-third is-offset-one-third m-t-100">
+    <div class="card">
+      <div class="card-content">
+        <h1 class="title">Forgot Password?</h1>
+
+        <form action="{{route('password.email')}}" method="POST" role="form">
+          {{csrf_field()}}
+          <div class="field">
+            <label for="email" class="label">Email Address</label>
+            <p class="control">
+              <input class="input {{$errors->has('email') ? 'is-danger' : ''}}" type="text" name="email" id="email" placeholder="name@example.com" value="{{old('email')}}">
+            </p>
+            @if ($errors->has('email'))
+              <p class="help is-danger">{{$errors->first('email')}}</p>
+            @endif
+          </div>
+
+          <button class="button is-primary is-outlined is-fullwidth m-t-30">Get Reset Link</button>
+        </form>
+      </div> <!-- end of .card-content -->
+    </div> <!-- end of .card -->
+    <h5 class="has-text-centered m-t-20"><a href="{{route('login')}}" class="is-muted"><i class="fa fa-caret-left m-r-10"></i> Back to Login</a></h5>
+  </div>
+</div>
+
+
+{{-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
-
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
@@ -14,7 +47,7 @@
                         </div>
                     @endif
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -43,5 +76,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
