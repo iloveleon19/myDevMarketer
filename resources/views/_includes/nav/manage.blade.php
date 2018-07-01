071e6607a45ca9c76b8e8c4086044a803f186cf1
iloveleon19 <iloveleon19@gmail.com>
2018-07-01 20:27:54 +0800

    ep-11

---
+++ resources/views/_includes/nav/manage.blade.php
@@ -0,0 +1,18 @@
+<div class="side-menu">
+    <aside class="menu m-t-30 m-l-10">
+        <p class="menu-lable">
+            General
+        </p>
+        <ul class="menu-list">
+            <li><a href="{{route('manage.dashboard')}}">Dashboard</a></li>
+        </ul>
+
+        <p class="menu-label">
+            Administation
+        </p>
+        <ul class="menu-list">
+            <li><a href="#">Manage Users</a></li>
+            <li><a href="#">Roles &amp; Permissions</a></li>
+        </ul>
+    </aside>
+</div>
